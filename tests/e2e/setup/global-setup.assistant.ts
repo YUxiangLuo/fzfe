eed7b6b4280e7fca5aebf619393c65cc66abdf68
YUxiangLuo <871884617@qq.com>
2026-03-10 23:42:08 +0800

    test(e2e): replace assistant mocks with real backend flows

--- tests/e2e/setup/global-setup.assistant.ts
+++ tests/e2e/setup/global-setup.assistant.ts
@@ -16,10 +16,16 @@ const BE_DIR = path.resolve(FE_DIR, "../be");
 export default async function globalSetup(_: FullConfig) {
   const assistantUsername = process.env.E2E_ASSISTANT_USERNAME ?? "assistant2";
   const assistantPassword = process.env.E2E_ASSISTANT_PASSWORD ?? "AssistantE2E!234";
+  const teacherUsername = process.env.E2E_TEACHER_USERNAME ?? "teacher1";
+  const teacherPassword = process.env.E2E_TEACHER_PASSWORD ?? "TeacherE2E!234";
+  const studentUsername = process.env.E2E_STUDENT_USERNAME ?? "20240001";
+  const studentPassword = process.env.E2E_STUDENT_PASSWORD ?? "StudentE2E!123";
 
   await resetDatabase(BE_DIR);
   await cleanupUploadArtifacts(BE_DIR);
   await resetUserPassword(BE_DIR, assistantUsername, assistantPassword);
+  await resetUserPassword(BE_DIR, teacherUsername, teacherPassword);
+  await resetUserPassword(BE_DIR, studentUsername, studentPassword);
   await seedAssistantCoreFixtures(BE_DIR);
   await seedEdgeCaseFixtures(BE_DIR);
 }
