3f8d7a6d8b37b7200e12e77fa50c278907bd3654
YuX <luoyuxiangqop@gmail.com>
2026-04-17 14:55:42 +0800

    add sleep

--- update.sh
+++ update.sh
@@ -1,17 +1,22 @@
 echo "===========FRONTEND GIT PULL==========="
 git pull
+sleep 2
 
 echo "===========run actions================"
 ./actions.sh
+sleep 2
 
 echo "=============VITE BUILD============"
 bunx vite build
+sleep 2
 
 echo "=============RSYNC DIST============="
 rsync --progress --delete -r ./dist/* /var/www/fangzhen/
+sleep 2
 
 echo "=============RSYNC NGINX CONFIG==========="
 sudo rsync --progress ./nginx-fangzhen /etc/nginx/sites-available/fangzhen
+sleep 2
 
 echo "=============RESTART NGINX=========="
 sudo systemctl restart nginx
