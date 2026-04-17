c0e2befac3c0f474ec30a232ec0e89d41949b716
YuX <luoyuxiangqop@gmail.com>
2026-04-17 14:47:56 +0800

    add nginx config and actions

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 echo "===========FRONTEND GIT PULL==========="
 git pull
 
+echo "===========run actions================"
+./actions.sh
+
 echo "=============VITE BUILD============"
 bunx vite build
 
 echo "=============RSYNC DIST============="
 rsync --progress --delete -r ./dist/* /var/www/fangzhen/
+
+echo "=============RSYNC NGINX CONFIG==========="
+sudo rsync --progress ./nginx-fangzhen /etc/nginx/sites-available/fangzhen
 
 echo "=============RESTART NGINX=========="
 sudo systemctl restart nginx
